dc40b7117a5e2295e52b6c1b9bc561130f9c8e78
Molly Sophia <mollysophia379@gmail.com>
2024-09-25 12:01:04 +0800

    AIMET quantization updates
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- export_quantized_model_encodings.py
+++ export_quantized_model_encodings.py
@@ -8,13 +8,16 @@ from transformers.tokenization_utils_base import BatchEncoding
 import types
 import torch
 import torch.nn as nn
+import onnx
 from aimet_torch.model_preparer import _prepare_traced_model
 
-from utils.model_utils import get_dummy_input_for_rwkv_causal_llm, get_input_output_names
+from utils.model_utils import get_dummy_input_for_rwkv_causal_llm, get_input_output_names, split_onnx
 from quantizers.base_quantizer import LLMQuantizer
 from utils.dataset_builder import DatasetBuilder
 
 import argparse
+import os
+import re
 from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Convert model')
@@ -25,6 +28,7 @@ parser.add_argument('--weights_bitwidth', type=int, default=8, help='Weights bit
 parser.add_argument('--use_cuda', action='store_true', default=True, help='Use CUDA')
 parser.add_argument('--test_generate', action='store_true', default=False, help='Test generate')
 parser.add_argument('--export_onnx', action='store_true', default=False, help='Export onnx and encoding files')
+parser.add_argument('--num_splits', type=int, default=2, help='Number of splits')
 args_parser = parser.parse_args()
 
 device = torch.device("cuda") if args_parser.use_cuda and torch.cuda.is_available() else torch.device("cpu")
@@ -62,7 +66,7 @@ args.eval_dataset_name = "wikitext"
 args.eval_dataset_config_name = "wikitext-103-raw-v1"
 args.eval_dataset_split = "test"
 args.eval_dataset_preprocessor = "gptq"
-args.num_calibration_batches = 20
+args.num_calibration_batches = 1
 args.per_device_calib_batch_size = 2
 args.per_device_eval_batch_size = 4
 args.block_size = 1024
@@ -87,6 +91,7 @@ model = model.to(device)
 model.rwkv.embeddings.weight = \
     nn.parameter.Parameter(data=nn.functional.layer_norm(model.rwkv.embeddings.weight, [model.config.hidden_size], \
                                 weight=model.rwkv.blocks[0].pre_ln.weight, bias=model.rwkv.blocks[0].pre_ln.bias, eps=1e-5))
+del model.rwkv.blocks[0].pre_ln
 tokenizer = AutoTokenizer.from_pretrained(str(args_parser.model), trust_remote_code=True)
 tokenizer.model_max_length = 1024
 
@@ -119,4 +124,89 @@ if args_parser.test_generate:
 if args_parser.export_onnx:
     input_names, output_names = get_input_output_names(model.config)
     quantizer.export_quantsim(dummy_input=dummy_input, input_names=input_names, output_names=output_names)
-# TODO: split the model
+
+    print("Post processing ONNX model")
+    onnx_path = os.path.join(args.export_dir, "onnx", f"{args.model_name}.onnx")
+    model = onnx.load(onnx_path, load_external_data=False)
+    graph = model.graph
+    nodes  = graph.node
+    pattern = r"blocks\.(\d+)"
+
+    for i in range(1, len(graph.input), 3):
+        graph.input[i].name = "layer" + str((i-1)//3) + "_state0_in"
+        graph.input[i+1].name = "layer" + str((i-1)//3) + "_state1_in"
+        graph.input[i+2].name = "layer" + str((i-1)//3) + "_state2_in"
+
+    for node_id, node in enumerate(nodes):
+        #print(node)
+        if "ln1/Add_1_output_0" in node.output[0]:
+            match = re.search(pattern, node.output[0])
+            number = match.group(1)
+            node.output[0] = "layer" + str(number) + "_state0_out"
+        elif "ln2/Add_1_output_0" in node.output[0]:
+            match = re.search(pattern, node.output[0])
+            number = match.group(1)
+            node.output[0] = "layer" + str(number) + "_state2_out"
+        elif len(node.input) > 0:
+            for id, _input in enumerate(node.input):
+                if "ln1/Add_1_output_0" in _input:
+                    match = re.search(pattern, node.input[id])
+                    number = match.group(1)
+                    node.input[id] = "layer" + str(number) + "_state0_out"
+                elif "ln2/Add_1_output_0" in _input:
+                    match = re.search(pattern, node.input[id])
+                    number = match.group(1)
+                    node.input[id] = "layer" + str(number) + "_state2_out"
+
+        if "attention/sub_shift/Sub_output_0" in node.output[0]:
+            match = re.search(pattern, node.output[0])
+            number = match.group(1)
+            print("original:", node.input[0])
+            node.input[0] = "layer" + str(number) + "_state0_in"
+            print("new:", node.input[0])
+        elif "feed_forward/sub_shifted/Sub_output_0" in node.output[0]:
+            match = re.search(pattern, node.output[0])
+            number = match.group(1)
+            print("original:", node.input[0])
+            node.input[0] = "layer" + str(number) + "_state2_in"
+            print("new:", node.input[0])
+        elif "add_time_first/Add_output_0" in node.output[0]:
+            match = re.search(pattern, node.output[0])
+            number = match.group(1)
+            print("original:", node.input[1])
+            node.input[1] = "layer" + str(number) + "_state1_in"
+            print("new:", node.input[1])
+        elif "mul_time_decay/Mul_output_0" in node.output[0]:
+            match = re.search(pattern, node.output[0])
+            number = match.group(1)
+            print("original:", node.input[1])
+            node.input[1] = "layer" + str(number) + "_state1_in"
+            print("new:", node.input[1])
+
+    state_in_dims = {}
+    for input in graph.input:
+        if "state0" in input.name:
+            state_in_dims["state0"] = input.type.tensor_type.shape
+        elif "state1" in input.name:
+            state_in_dims["state1"] = input.type.tensor_type.shape
+        elif "state2" in input.name:
+            state_in_dims["state2"] = input.type.tensor_type.shape
+    for idx, output in enumerate(graph.output):
+        if "state0" in output.name:
+            for i, dim in enumerate(state_in_dims["state0"].dim):
+                graph.output[idx].type.tensor_type.shape.dim[i].dim_value = dim.dim_value
+        elif "state1" in output.name:
+            for i, dim in enumerate(state_in_dims["state1"].dim):
+                graph.output[idx].type.tensor_type.shape.dim[i].dim_value = dim.dim_value
+        elif "state2" in output.name:
+            for i, dim in enumerate(state_in_dims["state2"].dim):
+                graph.output[idx].type.tensor_type.shape.dim[i].dim_value = dim.dim_value
+        elif "logits" in output.name:
+            graph.output[idx].type.tensor_type.shape.dim[0].dim_value = 1
+            graph.output[idx].type.tensor_type.shape.dim[1].dim_value = 1
+            graph.output[idx].type.tensor_type.shape.dim[2].dim_value = 65536
+
+    model.ir_version = 8
+    onnx.save_model(model, os.path.join(args.export_dir, "onnx", f"{args.model_name}_new.onnx"), save_as_external_data=True, all_tensors_to_one_file=True, size_threshold=1024, convert_attribute=False)
+
+    split_onnx(os.path.join(args.export_dir, "onnx", f"{args.model_name}_new.onnx"), args.model_name, args_parser.num_splits, args.export_dir, False)
--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,5 +1,11 @@
 import torch
 import contextlib
+import sys
+import os
+import pathlib
+import onnx
+import re
+from .split_onnx import OnnxSplitter, save_model
 
 model2config_lut = {
     'Rwkv6ForCausalLM': ('num_hidden_layers', 'num_attention_heads', 'attention_hidden_size'),
@@ -73,3 +79,159 @@ def get_input_output_names(model_cfg):
     output_names = ['logits']
     output_names += _get_state_names('out', num_layers)
     return input_names, output_names
+
+def _qnn_name(name, deco_digit=True):
+    name = f'_{name}' if deco_digit and name.isdigit() else name
+    #name = name.replace('.', '_')
+    name = name.replace('/', '-')
+    return name
+
+def get_onnx_input_output_names(onnxfile, onnxmodel=None, deco_digit=True):
+    onnxmodel = _load_model(onnxfile) if onnxmodel is None else onnxmodel
+    input_names = [_qnn_name(i.name, deco_digit=deco_digit) for i in onnxmodel.graph.input]
+    output_names = [_qnn_name(i.name, deco_digit=deco_digit) for i in onnxmodel.graph.output]
+    return input_names, output_names
+
+def _load_model(onnxfile, load_external_data=False, model_cache={}):
+    if onnxfile not in model_cache:
+        print(f'Loading {onnxfile}', file=sys.stderr)
+        model_cache[onnxfile] = onnx.load(onnxfile, load_external_data=load_external_data)
+    return model_cache[onnxfile]
+
+def split_onnx_by_names(onnxfile, modelname, *list_of_output_tensors, output_dir='.', onnxmodel=None):
+    onnxmodel = _load_model(onnxfile, load_external_data=False) if onnxmodel is None else onnxmodel
+    splitter = OnnxSplitter(onnxmodel, verbose=True)
+    base_dir = os.path.dirname(onnxfile)
+    using_external_data = OnnxSplitter.is_using_external_data(onnxmodel)
+
+    list_of_output_tensors = [i.split(',') for i in list_of_output_tensors]
+    num_splits = len(list_of_output_tensors) + 1
+    pathlib.Path(f'{output_dir}/split_onnx').mkdir(parents=True, exist_ok=True)
+
+    # 1. split model
+    new_model_info = []
+    for i, subgraph in enumerate(splitter.split(list_of_output_tensors)):
+        new_basename = f'{modelname}_{i+1}_of_{num_splits}'
+        input_tensor_names = [i.name for i in subgraph.input]
+        output_tensor_names = [i.name for i in subgraph.output]
+        new_model_info.append([new_basename, input_tensor_names, output_tensor_names])
+
+        submodel = onnx.helper.make_model(subgraph, opset_imports=onnxmodel.opset_import)
+        if not using_external_data and submodel.ByteSize() < onnx.checker.MAXIMUM_PROTOBUF:
+            onnx.checker.check_model(submodel)
+
+        if using_external_data:
+            onnx.load_external_data_for_model(submodel, base_dir=base_dir)
+
+        newonnxfile = f'{output_dir}/split_onnx/{new_basename}.onnx'
+        print(f'Saving {newonnxfile}')
+        save_model(submodel, newonnxfile, using_external_data)
+        return
+
+def _get_lm_head_sizes(onnxmodel):
+    lm_head_weight = [i for i in onnxmodel.graph.initializer if 'lm_head' in i.name and 'weight' in i.name]
+    if not lm_head_weight: #RWKV
+        lm_head_weight = [i for i in onnxmodel.graph.initializer if 'head' in i.name and 'weight' in i.name]
+    if len(lm_head_weight[0].dims) == 2:
+        embedding_size, vocab_size = lm_head_weight[0].dims
+    else:
+        vocab_size, embedding_size, _, _ = lm_head_weight[0].dims
+    return embedding_size, vocab_size
+
+def get_per_layer_name_formats(onnxnodes):
+    if all(i in onnxnodes for i in ('transformer.h.0.ln_1', 'transformer.ln_f')):
+        block_input, attn_output, ln_f = 'transformer.h.{}.ln_1', 'transformer.h.{}.ln_2', 'transformer.ln_f'
+    elif all(i in onnxnodes for i in ('model.layers.0.input_layernorm', 'model.norm')):
+        # these are for the legacy export of LLaMa model
+        block_input, attn_output, ln_f = 'model.layers.{}.input_layernorm', 'model.layers.{}.post_attention_layernorm', 'model.norm'
+    elif all(i in onnxnodes for i in ('model_layers_0_input_layernorm_Pow', 'model_norm_Pow')):
+        block_input, attn_output, ln_f = 'model_layers_{}_input_layernorm_Pow', 'model_layers_{}_post_attention_layernorm_Pow', 'model_norm_Pow'
+    elif all(i in onnxnodes for i in ('model.layers.0.input_layernorm.cast', 'model.norm.cast')):
+        # aimet onnx saver?
+        block_input, attn_output, ln_f = 'model.layers.{}.input_layernorm.cast', 'model.layers.{}.post_attention_layernorm.cast', 'model.norm.cast'
+    elif all(i in onnxnodes for i in ('/model/layers.0/input_layernorm/cast/Cast', '/model/norm/cast/Cast')):
+        # torch onnx export
+        block_input, attn_output, ln_f = '/model/layers.{}/input_layernorm/cast/Cast', '/model/layers.{}/post_attention_layernorm/cast/Cast', '/model/norm/cast/Cast'
+    elif all(i in onnxnodes for i in ('/model_layers_0_input_layernorm_Cast/Cast', '/model_norm_Cast/Cast')):
+        block_input, attn_output, ln_f = '/model_layers_{}_input_layernorm_Cast/Cast', '/model_layers_{}_post_attention_layernorm_Cast/Cast', '/model_norm_Cast/Cast'
+       # torch onnx export Aimet1.30 debug
+    elif all(i in onnxnodes for i in ('/model_layers_0_input_layernorm_Pow/Pow', '/model_norm_Pow/Pow')):
+        block_input, attn_output, ln_f = '/model_layers_{}_input_layernorm_Pow/Pow', '/model_layers_{}_post_attention_layernorm_Pow/Pow', '/model_norm_Pow/Pow'
+    elif all(i in onnxnodes for i in ('rwkv.blocks.0.ln1', 'rwkv.blocks.0.add_feed_forward')):
+        block_input, attn_output, ln_f = 'rwkv.blocks.{}.ln1', 'rwkv.blocks.{}.add_feed_forward', 'rwkv.ln_out'
+    else:
+        raise RuntimeError(f"Unexpected ONNX model, couldn't get the per-layer names")
+    return block_input, attn_output, ln_f
+
+def get_per_layer_output_names(onnxfile, onnxmodel=None, deco_digit=True, include_first_layer=False):
+    '''
+    Return the output tensor names of all transformer blocks in the model
+    '''
+    onnxmodel = _load_model(onnxfile) if onnxmodel is None else onnxmodel
+
+    per_layer_output_names = []
+
+    nodes = {i.name: i for i in onnxmodel.graph.node}
+
+    # new pipeline
+    fmt_node_name, _, ln_f = get_per_layer_name_formats(nodes)
+
+    start_from = 0 if include_first_layer else 1
+    for i in range(start_from, 10000): # skip the first because it's the beginning of the block
+        name = fmt_node_name.format(i)
+        if name not in nodes:
+            break
+        per_layer_output_names.append(nodes[name].input[0]) # use input
+
+    # add the end of the last transformer block, to have the same number names as the number of layers
+    assert ln_f in nodes, "Failed to find the end of the last transformer block"
+    per_layer_output_names.append(nodes[ln_f].input[0]) # use input
+
+    return per_layer_output_names
+
+
+def split_onnx(onnxfile, modelname, num_splits, output_dir='./', split_embedding=False):
+    def _is_rwkv_state(layer, name):
+        return re.search(f'layer{layer}_state', name) != None
+
+    num_splits = int(num_splits)
+
+    onnxmodel = _load_model(onnxfile, load_external_data=False)
+    input_names, output_names = get_onnx_input_output_names(onnxfile, onnxmodel=onnxmodel, deco_digit=False)
+    per_layer_output_names = get_per_layer_output_names(onnxfile, onnxmodel=onnxmodel, deco_digit=False, include_first_layer=split_embedding)
+    print('Per_layer_output_names:', per_layer_output_names)
+
+
+    # Infer the shape of per-layer tensors
+    input_ids, = [i for i in onnxmodel.graph.input if i.name == 'input_ids']
+    batch_size, seq_length = [i.dim_value for i in input_ids.type.tensor_type.shape.dim]
+
+    embedding_size, vocab_size = _get_lm_head_sizes(onnxmodel)
+    print(f'Using per-layer output shape: {[batch_size, seq_length, embedding_size]}')
+
+    per_layer_output_value_info = [onnx.helper.make_tensor_value_info(name, onnx.TensorProto.FLOAT, [batch_size, seq_length, embedding_size]) for name in per_layer_output_names]
+    onnxmodel.graph.value_info.extend(per_layer_output_value_info)
+
+    num_layers = len(per_layer_output_names)
+    num_layers_per_split = num_layers//num_splits
+    past_key_values = {layer:[output for output in output_names if _is_rwkv_state(layer, output)] for layer in range(num_layers)}
+
+
+    names_to_split = []
+    if split_embedding:
+        names_to_split.append(per_layer_output_names[0])
+        per_layer_output_names.pop(0)
+
+    num_layers = len(per_layer_output_names)
+    num_layers_per_split = (num_layers // (num_splits-1)) if split_embedding else (num_layers // num_splits)
+    past_key_values = {layer:[output for output in output_names if _is_rwkv_state(layer, output)] for layer in range(num_layers)}
+
+    for layer_end in range(num_layers_per_split,num_layers,num_layers_per_split):
+        outputs = [per_layer_output_names[layer_end-1]]
+        for layer in range(layer_end-num_layers_per_split, layer_end):
+            outputs += past_key_values[layer]
+        names_to_split.append(','.join(outputs))
+
+    print('Names_to_split', names_to_split)
+    assert num_splits == len(names_to_split)+1, f"Failed to split into {num_splits} pieces!"
+    return split_onnx_by_names(onnxfile, modelname, *names_to_split, output_dir=output_dir, onnxmodel=onnxmodel)
