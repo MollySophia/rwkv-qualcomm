d3b62ea90fd99cdd08f0a9195f5b6b6540dfc017
Molly Sophia <mollysophia379@gmail.com>
2024-10-06 10:58:45 +0800

    Process context binaries for chunked models together
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- make_context_cache_binary.py
+++ make_context_cache_binary.py
@@ -17,16 +17,35 @@ def main():
     old_qnn = True if QNN_VERSION_MINOR < 22 else False
     print(f"QNN_VERSION_MINOR: {QNN_VERSION_MINOR}")
 
-    model_name = str(args.model_lib).split('/')[-1].replace('.so', '')
-    dump_htp_config(args.platform, [model_name], str(args.model_lib).replace('.so', '_htp_config.json'), old_qnn)
-    dump_htp_link_config(str(args.model_lib).replace('.so', '_htp_link.json'), qnn_sdk_root)
-    convert_cmd = f"{qnn_sdk_root}/bin/x86_64-linux-clang/qnn-context-binary-generator"
-    convert_cmd += f" --backend {qnn_sdk_root}/lib/x86_64-linux-clang/libQnnHtp.so"
-    convert_cmd += f" --model {args.model_lib}"
-    convert_cmd += f" --output_dir {args.output_path}"
-    convert_cmd += f" --binary_file {model_name.replace('lib', '')}"
-    convert_cmd += f" --config_file {str(args.model_lib).replace('.so', '_htp_link.json')}"
-    os.system(convert_cmd)
+    if "chunk" in str(args.model_lib):
+        print("Chunked model detected")
+        num_chunks = int(str(args.model_lib).split('chunk')[-1].replace('.so', '').split('of')[-1])
+        print(f"Number of chunks: {num_chunks}")
+        for i in range(1, num_chunks+1):
+            model_path = str(args.model_lib).split('chunk')[0] + f"chunk{i}of{num_chunks}.so"
+            print(f"Processing chunk {model_path}")
+            model_name = model_path.split('/')[-1].replace('.so', '')
+            dump_htp_config(args.platform, [model_name], model_path.replace('.so', '_htp_config.json'), old_qnn)
+            dump_htp_link_config(model_path.replace('.so', '_htp_link.json'), qnn_sdk_root)
+            convert_cmd = f"{qnn_sdk_root}/bin/x86_64-linux-clang/qnn-context-binary-generator"
+            convert_cmd += f" --backend {qnn_sdk_root}/lib/x86_64-linux-clang/libQnnHtp.so"
+            convert_cmd += f" --model {model_path}"
+            convert_cmd += f" --output_dir {args.output_path}"
+            convert_cmd += f" --binary_file {model_name.replace('lib', '')}"
+            convert_cmd += f" --config_file {model_path.replace('.so', '_htp_link.json')}"
+            os.system(convert_cmd)
+
+    else:
+        model_name = str(args.model_lib).split('/')[-1].replace('.so', '')
+        dump_htp_config(args.platform, [model_name], str(args.model_lib).replace('.so', '_htp_config.json'), old_qnn)
+        dump_htp_link_config(str(args.model_lib).replace('.so', '_htp_link.json'), qnn_sdk_root)
+        convert_cmd = f"{qnn_sdk_root}/bin/x86_64-linux-clang/qnn-context-binary-generator"
+        convert_cmd += f" --backend {qnn_sdk_root}/lib/x86_64-linux-clang/libQnnHtp.so"
+        convert_cmd += f" --model {args.model_lib}"
+        convert_cmd += f" --output_dir {args.output_path}"
+        convert_cmd += f" --binary_file {model_name.replace('lib', '')}"
+        convert_cmd += f" --config_file {str(args.model_lib).replace('.so', '_htp_link.json')}"
+        os.system(convert_cmd)
 
 if __name__ == '__main__':
     main()
