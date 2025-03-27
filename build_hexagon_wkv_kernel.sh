49818ae47690d17a5c4a3b1b6bee54be28d3cb6d
Molly Sophia <mollysophia379@gmail.com>
2025-03-27 13:37:30 +0800

    v68 v69 v73 customop
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- build_hexagon_wkv_kernel.sh
+++ build_hexagon_wkv_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf hexagon/HTP/RwkvWkvOpPackage/build
-make -C hexagon/HTP/RwkvWkvOpPackage/ htp_x86 htp_v75 -j4
+make -C hexagon/HTP/RwkvWkvOpPackage/ htp_x86 htp_v68 htp_v69 htp_v73 htp_v75 -j4
 
 make -C hexagon/CPU/RwkvWkvOpPackage/ -j4
