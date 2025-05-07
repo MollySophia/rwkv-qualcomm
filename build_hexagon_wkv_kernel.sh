52f3fd78a55db1d96dcc678cf3faed291b5ab266
Molly Sophia <mollysophia379@gmail.com>
2025-05-07 09:00:58 +0800

    [untested] add htp v79 target for hexagon customop
    Signed-off-by: Molly Sophia <mollysophia379@gmail.com>

--- build_hexagon_wkv_kernel.sh
+++ build_hexagon_wkv_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf hexagon/HTP/RwkvWkvOpPackage/build
-make -C hexagon/HTP/RwkvWkvOpPackage/ htp_x86 htp_v68 htp_v69 htp_v73 htp_v75 -j4
+make -C hexagon/HTP/RwkvWkvOpPackage/ htp_x86 htp_v68 htp_v69 htp_v73 htp_v75 htp_v79 -j4
 
 make -C hexagon/CPU/RwkvWkvOpPackage/ -j4
 
@@ -12,3 +12,4 @@ cp hexagon/HTP/RwkvWkvOpPackage/build/hexagon-v68/libQnnRwkvWkvOpPackage.so hexa
 cp hexagon/HTP/RwkvWkvOpPackage/build/hexagon-v69/libQnnRwkvWkvOpPackage.so hexagon/HTP/prebuilt/libQnnRwkvWkvOpPackageV69.so
 cp hexagon/HTP/RwkvWkvOpPackage/build/hexagon-v73/libQnnRwkvWkvOpPackage.so hexagon/HTP/prebuilt/libQnnRwkvWkvOpPackageV73.so
 cp hexagon/HTP/RwkvWkvOpPackage/build/hexagon-v75/libQnnRwkvWkvOpPackage.so hexagon/HTP/prebuilt/libQnnRwkvWkvOpPackageV75.so
+cp hexagon/HTP/RwkvWkvOpPackage/build/hexagon-v79/libQnnRwkvWkvOpPackage.so hexagon/HTP/prebuilt/libQnnRwkvWkvOpPackageV79.so
